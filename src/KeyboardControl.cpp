6559fe28ae3e78cd16f6d5085f2e99bd82c22d8d
puncochar <puncochar@Ubunntu.myguest.virtualbox.org>
2026-03-17 16:17:54 +0100

    Lab 04: add keyboard control node

--- src/KeyboardControl.cpp
+++ src/KeyboardControl.cpp
@@ -1,21 +1,70 @@
 #include <chrono>
 #include <functional>
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/select.h>
+
 #include "KeyboardControl.hpp"
 
 using namespace std::chrono_literals;
 
-KeyboardControlNode::KeyboardControlNode(): rclcpp::Node("keyboard_control_node") {
+KeyboardControlNode::KeyboardControlNode() : rclcpp::Node("keyboard_control_node") {
+    this->declare_parameter("linear_speed", 0.5);
+    this->declare_parameter("angular_speed", 0.5);
+
+    linear_speed_ = this->get_parameter("linear_speed").as_double();
+    angular_speed_ = this->get_parameter("angular_speed").as_double();
 
     twist_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    
+
     timer_ = this->create_wall_timer(10ms, std::bind(&KeyboardControlNode::timerCallback, this));
 
+    tcgetattr(STDIN_FILENO, &old_termios_);
+    struct termios new_termios = old_termios_;
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+
     RCLCPP_INFO(get_logger(), "Keyboard Control node started.");
+    RCLCPP_INFO(get_logger(), "Use arrow keys to control robot.");
 }
 
 KeyboardControlNode::~KeyboardControlNode() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_termios_);
 }
 
 void KeyboardControlNode::timerCallback() {
+    geometry_msgs::msg::Twist twist{};
+    char c;
+
+    if (read(STDIN_FILENO, &c, 1) == 1) {
+        if (c == '\033') {
+            char seq1, seq2;
+
+            if (read(STDIN_FILENO, &seq1, 1) == 1 && read(STDIN_FILENO, &seq2, 1) == 1) {
+                if (seq1 == '[') {
+                    switch (seq2) {
+                        case 'A':
+                            twist.linear.x = linear_speed_;
+                            break;
+                        case 'B':
+                            twist.linear.x = -linear_speed_;
+                            break;
+                        case 'C':
+                            twist.angular.z = -angular_speed_;
+                            break;
+                        case 'D':
+                            twist.angular.z = angular_speed_;
+                            break;
+                        default:
+                            return;
+                    }
+
+                    twist_publisher_->publish(twist);
+                }
+            }
+        }
+    }
 }
